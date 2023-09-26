e0500420ff12e9878f111c8fb7e7e5d5856302ac
Neil Cutcliffe <neil@Neils-Desktop.lan>
2023-09-26 12:07:00 -0700

    More steps towards converting the Quest project to Vite.

--- src/root.jsx
+++ src/root.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import {
   Outlet,
   NavLink,
@@ -10,23 +10,29 @@ import {
   redirect,
   Form,
 } from "react-router-dom";
-import { getPosts, getMedia } from "./components/wpapi/posts";
-import Card4 from './components/Card4/Card4';
+//import { getPosts, getMedia } from "./components/wpapi/posts";
+import SampleCard from './components/SampleCard/SampleCard';
 
 
 export async function loader({ request }) {
   console.log('rootLoader()');
-  const posts = await getPosts();
-  const media = await getMedia(posts[0].featured_media);
-  return { posts, media };
+//  const posts = await getPosts();
+  const post =  {
+    title   : 'Title Neil 3',
+    content : 'Content Neil was here two.',
+    image   : 'url(https://babyapps.io/test-1/wp-content/uploads/sites/14/2023/01/8344BA1F-CBA1-4E95-846C-66C913A43E30-2-scaled.jpeg)'
+  };
+  return { post };
+//  const media = await getMedia(posts[0].featured_media);
+//  return { posts, media };
 }
 
 export default function Root() {
   console.log('Root()');
-  const { posts, media } = useLoaderData();
-  const navigation = useNavigation();
+  const { post } = useLoaderData();
+//  const navigation = useNavigation();
 
   return (
-    <Card4 post={posts[0]} media={media}/>
+    <SampleCard post={post} />
   );
 }
